ece27b01249fb72673a834855b07b7358b5441a8
Yev Dyko <evgeny.dyko@gmail.com>
2017-05-12 12:49:14 +0300

    Add user stories to displaying tasks spec

--- spec/features/user_views_tasks_spec.rb
+++ spec/features/user_views_tasks_spec.rb
@@ -1,23 +1,23 @@
 require 'rails_helper'
 
 feature 'User views tasks' do
+  # As a User
+  # So that I can know the name of app I'm using
+  # I want to view an app title
   scenario 'can see an app title on the homepage', js: true do
     visit root_path
 
     expect(page).to have_content t('tasks.title')
   end
 
-  scenario 'displays a prompt to add a task', js: true do
-    visit root_path
-
-    expect(page).to have_button t('tasks.new')
-  end
-
+  # As a User
+  # So that I can know how many tasks I need to do
+  # I want to see a list of tasks
   scenario 'can see all tasks on the homepage', js: true do
     create_list(:task, 5)
     
     visit root_path
-    
+
     expect(page).to have_displayed_tasks(5)
   end
 
