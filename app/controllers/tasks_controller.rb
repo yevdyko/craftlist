43f3cec0543a9092b45ff4ecd3d20945002f7637
Yev Dyko <evgeny.dyko@gmail.com>
2017-05-14 12:28:23 +0300

    Add getting data from client to server

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,20 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
+  def create
+    task = Task.new(task_params)
+
+    if task.save
+      render json: task, status: :created
+    else
+      render json: { errors: task.errors }, status: :unproccesable_entity
+    end
+  end
+
+  private
+
+    def task_params
+      params.require(:task).permit(:description, :completed)
+    end
 end
--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -1,4 +1,46 @@
 require 'rails_helper'
 
 describe TasksController, type: :controller do
+  describe 'POST #create' do
+    let(:json) { JSON.parse(response.body) }
+
+    context 'with valid attributes' do
+      let(:task_attrs) { attributes_for :task }
+      
+      before do
+        post :create, params: { task: task_attrs }
+      end
+
+      it 'creates a new task' do
+        expect {
+          post :create, params: { task: task_attrs }
+        }.to change(Task, :count).by(1)
+      end
+      
+      it 'returns 201 as status code' do
+        expect(response).to have_http_status(:created)
+      end
+    end
+
+    context 'with invalid attributes' do
+      let(:invalid_task_attrs) { attributes_for(:task, description: nil) }
+
+      before do
+        post :create, params: { task: invalid_task_attrs }
+      end
+
+      it 'does not save the new task' do
+        expect {
+          post :create, params: { task: invalid_task_attrs }
+        }.to_not change(Task, :count)
+      end
+
+      it 'returns error message' do
+        task = build(:task, description: nil)
+        task.validate
+
+        expect(json['errors']).to eq(task.errors.messages.as_json)
+      end
+    end
+  end
 end
